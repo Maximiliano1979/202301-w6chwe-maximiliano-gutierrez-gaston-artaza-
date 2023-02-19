3ffb6136b240e815b56f31c463be7559ffe8472c
Gaston82 <cabezaartaza@hotmail.com>
2023-02-19 18:34:09 +0100

    Add a new mongoose set

--- src/database/config.ts
+++ src/database/config.ts
@@ -4,6 +4,12 @@ const logger = debug("robots:root");
 
 const dataBaseConnection = async (url: string) => {
   mongoose.set("strictQuery", false);
+  mongoose.set("toJSON", {
+    virtuals: true,
+    transform(doc, converted) {
+      delete converted._id;
+    },
+  });
   try {
     await mongoose.connect(url);
     logger("Base de datos online");
--- src/database/models/robotSchema.ts
+++ src/database/models/robotSchema.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 import mongoose, { Schema } from "mongoose";
 
 const robotSchema = new Schema({
@@ -8,14 +7,6 @@ const robotSchema = new Schema({
   speed: Number,
   endurance: Number,
 });
-
-robotSchema.methods.toJSON = function () {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-  const { _id, ...robot } = this.toObject();
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  robot.id = _id;
-  return robot;
-};
 
 const Robot = mongoose.model("Robot", robotSchema);
 

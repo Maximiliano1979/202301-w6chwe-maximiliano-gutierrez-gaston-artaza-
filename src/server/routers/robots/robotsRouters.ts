0ffce94993fe6902f10304100f9349f071c8c989
Gaston82 <cabezaartaza@hotmail.com>
2023-02-20 19:16:49 +0100

    Create startServer file

--- src/server/routers/robots/robotsRouters.ts
+++ src/server/routers/robots/robotsRouters.ts
@@ -7,7 +7,6 @@ import {
   updateRobotName,
 } from "../../controllers/robots/robotsControllers.js";
 
-// eslint-disable-next-line new-cap
 const robotsRouters = Router();
 
 robotsRouters.get("/", getRobots);
